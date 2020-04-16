a03c57ed498f4c1e50708843b5041b7cfddffd8f
Alan Budiarto <alanwbudiarto@gmail.com>
2020-04-16 22:17:22 +0700

    Tile Creation

---
+++ client/src/components/Tile.vue
@@ -0,0 +1,40 @@
+<template>
+	<td class="tile" @click="check">{{ symbol }}</td>
+</template>
+
+<script>
+	export default {
+		name: "Tile",
+		props: {
+			activeSymbol: String,
+			playerSymbol: String,
+			tile: Number,
+			room: String
+		},
+		data () {
+			return {
+				symbol: ''
+			}	
+		},
+		methods: {
+			check: function() {
+				if (this.symbol == '' && this.playerSymbol == this.activeSymbol) {
+					this.symbol = this.activeSymbol;
+					playTurn();
+				}
+				else {
+
+				}
+			},
+			playTurn() {
+				this.$socket.emit('playTurn', {
+					tile: this.tile,
+					room: this.room
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
