7130e870a1401d8e3171e63e7103b7de247ee16d
muhammadKholid <muhamma4dkhal3d@gmail.com>
2020-04-16 19:25:52 +0700

    namabahin 1 player dan 2 player dalam socket io

--- index.js
+++ index.js
@@ -19,6 +19,18 @@ io.on('connection', (socket) => {
     socket.join(`room-${++rooms}`);
     socket.emit('newGame', { name: data.name, room: `room-${rooms}` });
   });
+
+  // Connect the Player 2 to the room he requested. Show error if room full.
+  socket.on('joinGame', function (data) {
+    var room = io.nsps['/'].adapter.rooms[data.room];
+    if (room && room.length === 1) {
+      socket.join(data.room);
+      socket.broadcast.to(data.room).emit('player1', {});
+      socket.emit('player2', { name: data.name, room: data.room });
+    } else {
+      socket.emit('err', { message: 'Sorry, The room is full!' });
+    }
+  });
 });
 
 server.listen(process.env.PORT || 5000);
