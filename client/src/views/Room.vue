4fdf06a0b7230c6bf88bcde586ab9fa01ae5debe
Alan Budiarto <alanwbudiarto@gmail.com>
2020-04-17 02:34:47 +0700

    Rooms Init

---
+++ client/src/views/Room.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="contentDashboard">
+        <h2 class="title">Welcome, </h2>
+        <hr>
+        <div class="container">
+            <Board />
+        </div>
+    </div>
+</template>
+<script>
+import Board from '@/components/Board.vue';
+export default {
+    name: 'Room',
+    components: {
+    },
+    data () {
+        return {
+        }
+    },
+    methods: {
+    },
+
+}
+</script>
+
+<style>
+    .contentDashboard {
+      padding-top: 50px;
+    }
+</style>
