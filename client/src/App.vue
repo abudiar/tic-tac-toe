0eb5e6d02d9c179e77549287546684548d78a9c0
Claudio Antonius <antonius.claudio@gmail.com>
2020-04-16 21:33:07 +0700

    views Dashboard

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="app">
+    <!--
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
+    -->
+    <Dashboard></Dashboard>
   </div>
 </template>
+<script>
+import Dashboard from './views/Dashboard'
+export default {
+  components: {
+    Dashboard
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
---
+++ client/src/views/Dashboard.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div class="boxs">
+            <h1>Tic Tac Toe</h1>
+            <hr>
+            <form >
+                <input class="btn btn-primary" type="text" placeholder="Nickname">
+                <button type="submit" class="btn btn-primary">Play Game</button>
+            </form>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Dashboard',
+    data () {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style>
+    .boxs {
+        background-color: #cbecdd7d;
+        padding: 30px;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        box-sizing: border-box;
+        position: absolute;
+    }
+</style>
