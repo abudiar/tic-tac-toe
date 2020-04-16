e2a16d90b22062f664a37d7488e2a2aae976a8ff
muhammadKholid <muhamma4dkhal3d@gmail.com>
2020-04-16 22:04:27 +0700

    benerin typo sama tambah function

--- server/socket.js
+++ server/socket.js
@@ -1,10 +1,10 @@
 module.exports = {
   sockets(socket) {
     function makeid(length) {
-      let result = '';
-      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-      const charactersLength = characters.length;
-      for (let i = 0; i < length; i++) {
+      var result = '';
+      var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      var charactersLength = characters.length;
+      for (var i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
       }
       return result;
@@ -15,6 +15,10 @@ module.exports = {
       socket.join(room);
       socket.emit('newGame', { name: data.name, room });
       games[room] = { p1: data.name };
+      socket.broadcast.to('rooms').emit({
+        room,
+        p1: data.name,
+      });
     });
     socket.on('joinGame', function (data) {
       var room = io.nsps['/'].adapter.rooms[data.room];
@@ -22,7 +26,11 @@ module.exports = {
         socket.join(data.room);
         socket.broadcast.to(data.room).emit('player1', {});
         socket.emit('player2', { name: data.name, room: data.room });
-        games[room] = { p2: data.name };
+        games[data.room] = { p2: data.name };
+        socket.broadcast.to('rooms').emit({
+          room,
+          p2: data.name,
+        });
       } else {
         socket.emit('err', { message: 'Sorry, The room is full!' });
       }
@@ -32,11 +40,20 @@ module.exports = {
         tile: data.tile,
         room: data.room,
       });
-      games[room] = { tile: data.tile };
+      if (!games[data.room]['tiles']) games[data.room]['tiles'] = [];
+      games[data.room]['tiles'].push(data.tile);
+      socket.broadcast.to('rooms').emit({
+        room,
+        tiles: games[data.room]['tiles'],
+      });
     });
     socket.on('gameEnded', (data) => {
       socket.broadcast.to(data.room).emit('gameEnd', data);
-      games[room] = { endMessage: data.message };
+      games[data.room] = { endMessage: data.message };
+      socket.broadcast.to('rooms').emit({
+        room,
+        endMessage: data.message,
+      });
     });
     socket.on('rooms', () => {
       socket.emit('rooms', games);
