8930c3f1dfc70f16894c92a97eb0e94a06dba39f
Alan Budiarto <alanwbudiarto@gmail.com>
2020-04-17 02:49:10 +0700

    Added Socket Client

--- client/src/main.js
+++ client/src/main.js
@@ -4,10 +4,21 @@ import router from './router'
 import store from './store'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
+import VueSocketIO from 'vue-socket.io'
+Vue.config.productionTip = false
 
 Vue.use(Buefy)
 
-Vue.config.productionTip = false
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: 'http://localhost:3000',
+  vuex: {
+      store,
+      actionPrefix: 'SOCKET_',
+      mutationPrefix: 'SOCKET_'
+  },
+  options: { path: "/" }
+}))
 
 new Vue({
   router,
