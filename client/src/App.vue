730fc80d62c97e166f2f6a4b39a1362f7ddf2464
Claudio Antonius <antonius.claudio@gmail.com>
2020-04-17 09:15:37 +0700

    delete unnecessary

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,20 +1,11 @@
 <template>
   <div id="app">
-    <!--
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <Dashboard></Dashboard>
-    -->
     <router-view/>
   </div>
 </template>
 <script>
-// import Dashboard from './views/Dashboard'
 export default {
   components: {
-    // Dashboard
   }
 }
 </script>
